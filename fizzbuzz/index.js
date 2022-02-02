8bd3a2ce738b43bcf71e64d2fa82636d08ed1875
Bryan Dang <bryanvdang@gmail.com>
2022-02-02 11:44:35 -0700

    fizzbuzz practice finished

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -12,6 +12,20 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
 
-module.exports = fizzBuzz;
+function fizzbuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz(15);
+// module.exports = fizzBuzz;
